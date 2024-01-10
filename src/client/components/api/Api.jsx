1513a2e5f4d90adf430bee43669884965e37f3bd
Sodiqjon <134689903+Sodiqjon09@users.noreply.github.com>
2024-01-10 17:58:10 +0500

    git push

--- src/client/components/api/Api.jsx
+++ src/client/components/api/Api.jsx
@@ -1,30 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
 import Like from "../Like/Like";
 import "./Car_api.css";
 
 const Api = () => {
   const [data, setData] = useState([]);
-  const [datas, setDatas] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://themealdb.com/api/json/v1/1/filter.php?a=Canadian")
-      .then((response) => response.json())
-      .then((json) => {
-        setDatas(json.meals);
+    fetch("https://masterphoneuz.pythonanywhere.com/")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(json => {
+        setData(json);
         setLoading(false);
       })
-      .catch((error) => {
+      .catch(error => {
         console.error("Error fetching data:", error);
         setLoading(false);
       });
-  }, []);
-
-  useEffect(() => {
-    fetch("https://masterphoneuz.pythonanywhere.com/")
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   return (
@@ -62,36 +59,36 @@ const Api = () => {
           </div>
         </div>
       ) : (
-        datas?.map((car) => (
-          <div id="apiimgText" key={car.idMeal}>
+        data?.map((item) => (
+          <div id="apiimgText" key={item.id}>
             <div>
-              <img src={car.strMealThumb} alt="" />
+                <img src={`https://masterphoneuz.pythonanywhere.com${item.photo[0].photo}`} alt="" />
             </div>
             <div id="carUz">
-              {data.map((item) => (
-                <div key={item.id}>
-                  <div id="carNameText">
-                    <div id="model">
+              <div>
+                <div id="carNameText">
+                  <div id="model">
                     <h1>{item.model.name}</h1>
                     <Like />
-                    </div>
-                    <div id="cost" >
-                    <p><span>Narxi:</span> <span>{item.narhi}</span></p>
-                    </div>
                   </div>
-                  <div id="apiText">
-                    <p>{item.yili}</p>
-                    <p>{item.kraska_holati}</p>
-                    <p>{item.data}</p>
-                    <p>{item.yana}</p>
-                    <p>{item.rusum.name}</p>
-                    <p>{item.shahar.name}</p>
-                    <p>{item.yeyishi.name}</p>
-                    <p>{item.karobka.name}</p>
-                    <p>{item.rang.name}</p>
+                  <div id="cost">
+                    <p>
+                      <span>Narxi:</span> <span>{item.narhi} so`m</span>
+                    </p>
                   </div>
                 </div>
-              ))}
+                <div id="apiText">
+                  <p>{item.yili}</p>
+                  <p>{item.kraska_holati}</p>
+                  <p>{item.data}</p>
+                  <p>{item.yana}</p>
+                  <p>{item.rusum.name}</p>
+                  <p>{item.shahar.name}</p>
+                  <p>{item.yeyishi.name}</p>
+                  <p>{item.karobka.name}</p>
+                  <p>{item.rang.name}</p>
+                </div>
+              </div>
             </div>
           </div>
         ))
